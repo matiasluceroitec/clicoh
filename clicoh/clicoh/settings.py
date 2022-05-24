18776fc6c00e7a1fbbba89b14c9b6e40fe160f43
matiasjavierlucero <matiasjavierlucero@gmail.com>
2022-05-24 11:32:45 -0300

    add migrations

--- clicoh/clicoh/settings.py
+++ clicoh/clicoh/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=9lp+o1i#i3$4qi)c+k9b3r6rcs=ayfyfcfmf%5*@^=ubnwbo1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -136,9 +136,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
